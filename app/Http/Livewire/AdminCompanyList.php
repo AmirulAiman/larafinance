4e5b7defe3d89923baf236b6d6d3a41aa91a5680
Amirul Aiman <aiman_amirul@ymail.com>
2020-09-29 14:26:44 +0800

    add edit company modal

--- app/Http/Livewire/AdminCompanyList.php
+++ app/Http/Livewire/AdminCompanyList.php
@@ -3,12 +3,42 @@
 namespace App\Http\Livewire;
 
 use App\Models\Company;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class AdminCompanyList extends Component
 {
     public $search;
+    public $edit = false;
+
     public $companies = [];
+
+    public $company_id;
+    public $company_name;
+    public $address1;
+    public $address2;
+    public $postcode;
+    public $city;
+    public $state;
+
+    public $name;
+    public $email;
+    public $username;
+    private $password = '1234567890';
+    private $role_id = 2;
+
+    protected $rules = [
+        'company_name' =>'required',
+        'address1' => 'required',
+        'postcode' => 'required',
+        'city' => 'required',
+        'state' => 'required',
+        'name' =>'required',
+        'email' =>'required|unique:users,email',
+        'username' =>'required|unique:users,username',
+    ];
+
     public function render()
     {
         $this->companies = Company::where('company_name','like','%'.$this->search.'%')->get();
@@ -17,12 +47,82 @@ class AdminCompanyList extends Component
 
     private function clearForm()
     {
+        $this->company_name = '';
+        $this->address1 = '';
+        $this->address2 = '';
+        $this->postcode = '';
+        $this->city = '';
+        $this->state = '';
 
+        $this->name = '';
+        $this->email = '';
+        $this->username = '';
     }
 
     public function newCompany()
     {
+        $this->validate();
+        $company = Company::create([
+            'company_name' => $this->company_name,
+            'address1' => $this->address1,
+            'address2' => $this->address2,
+            'postcode' => $this->postcode,
+            'city' => $this->city,
+            'state' => $this->state
+        ]);
 
+        $user = User::create([
+           'name' => $this->name,
+           'username' => $this->username,
+           'email' => $this->email,
+           'password' => Hash::make($this->password),
+           'password_unhash' => $this->password,
+            'role_id' => $this->role_id
+        ]);
+
+        $company->users()->save($user);
+        $this->clearForm();
+        $this->closeModal();
+    }
+
+    public function editCompany(Company $company)
+    {
+        $this->edit = true;
+        $this->company_id = $company->id;
+        $this->company_name = $company->company_name;
+        $this->address1 = $company->address1;
+        $this->address2 = $company->address2;
+        $this->postcode = $company->postcode;
+        $this->city = $company->city;
+        $this->state = $company->state;
+
+        $this->name = $company->users[0]->name;
+        $this->email = $company->users[0]->email;
+        $this->username = $company->users[0]->username;
+        $this->openModal();
+    }
+
+    public function updateCompany()
+    {
+        $company = Company::find($this->company_id);
+        $user = $company->users[0];
+
+        $company->company_name = $this->company_name;
+        $company->address1 = $this->address1;
+        $company->address2 =  $this->address2;
+        $company->postcode = $this->postcode;
+        $company->city = $this->city;
+        $company->state = $this->state;
+        $company->save();
+
+        $user->name = $this->name;
+        $user->username = $this->username;
+        $user->email = $this->email;
+        $user->save();
+
+        $this->closeModal();
+        $this->clearForm();
+        $this->edit = false;
     }
 
     public function openModal()
@@ -34,4 +134,5 @@ class AdminCompanyList extends Component
     {
         $this->dispatchBrowserEvent('closeModal');
     }
+
 }
--- resources/views/livewire/admin-company-list.blade.php
+++ resources/views/livewire/admin-company-list.blade.php
@@ -1,6 +1,269 @@
 <div class="card">
-    <div class="card-header">
+    <div class="card-header d-flex justify-content-between">
         <h3>List of Company Registered.</h3>
+        <button
+            type="button"
+            class="btn btn-primary"
+            wire:click="openModal"
+        >
+            <span class="fas fa-plus"></span>
+        </button>
+        <!-- The Modal -->
+        <div wire:ignore.self  class="modal fade" id="newCompany">
+            @if($edit)
+                <form wire:submit.prevent="updateCompany">
+                    <input type="hidden" wire:model="company_id">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">Update Company</h4>
+                                <button
+                                    type="button"
+                                    class="close"
+                                    wire:click="closeModal"
+                                >
+                                    <span class="fas fa-times"></span>
+                                </button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="company_name">Company Name: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="company_name"
+                                    />
+                                    @error('company_name') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="address1">Address1: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="address1"
+                                    />
+                                    @error('address1') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="address2">Address2: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="address2"
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="postcode">Postcode: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="postcode"
+                                    />
+                                    @error('postcode') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="city">City: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="city"
+                                    />
+                                    @error('city') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="state">Company Name: </label>
+                                    <select name="state" id="state" class="form-control">
+                                        <option value="">Select state...</option>
+                                        <option value="johor">Johor</option>
+                                        <option value="kedah">Kedah</option>
+                                        <option value="kelantan">Kelantan</option>
+                                        <option value="melaka">Melaka</option>
+                                        <option value="negeri sembilan">Negeri Sembilan</option>
+                                        <option value="pahang">Pahang</option>
+                                        <option value="penang">Penang</option>
+                                        <option value="perak">Perak</option>
+                                        <option value="perlis">Perlis</option>
+                                        <option value="sabah">Sabah</option>
+                                        <option value="sarawak">Sarawak</option>
+                                        <option value="selangor">Selangor</option>
+                                        <option value="terengganu">Terengganu</option>
+                                        <option value="wilayah persekutuan">Wilayah Persekutuan</option>
+                                    </select>
+                                    @error('state') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <hr>
+                                <div class="form-group">
+                                    <label for="company_name">Person in Charge: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="name"
+                                    />
+                                    @error('name') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="company_name">Email: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="email"
+                                    />
+                                    @error('email') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="company_name">Username: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="username"
+                                    />
+                                    @error('username') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+
+                            </div>
+
+                            <!-- Modal footer -->
+                            <div class="modal-footer">
+                                <button
+                                    type="submit"
+                                    class="btn btn-primary"
+                                >Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            @else
+                <form wire:submit.prevent="newCompany">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">New Company</h4>
+                                <button
+                                    type="button"
+                                    class="close"
+                                    wire:click="closeModal"
+                                >
+                                    <span class="fas fa-times"></span>
+                                </button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="company_name">Company Name: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="company_name"
+                                    />
+                                    @error('company_name') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="address1">Address1: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="address1"
+                                    />
+                                    @error('address1') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="address2">Address2: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="address2"
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="postcode">Postcode: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="postcode"
+                                    />
+                                    @error('postcode') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="city">City: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="city"
+                                    />
+                                    @error('city') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="state">Company Name: </label>
+                                    <select name="state" id="state" class="form-control">
+                                        <option value="">Select state...</option>
+                                        <option value="johor">Johor</option>
+                                        <option value="kedah">Kedah</option>
+                                        <option value="kelantan">Kelantan</option>
+                                        <option value="melaka">Melaka</option>
+                                        <option value="negeri sembilan">Negeri Sembilan</option>
+                                        <option value="pahang">Pahang</option>
+                                        <option value="penang">Penang</option>
+                                        <option value="perak">Perak</option>
+                                        <option value="perlis">Perlis</option>
+                                        <option value="sabah">Sabah</option>
+                                        <option value="sarawak">Sarawak</option>
+                                        <option value="selangor">Selangor</option>
+                                        <option value="terengganu">Terengganu</option>
+                                        <option value="wilayah persekutuan">Wilayah Persekutuan</option>
+                                    </select>
+                                    @error('state') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <hr>
+                                <div class="form-group">
+                                    <label for="company_name">Person in Charge: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="name"
+                                    />
+                                    @error('name') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="company_name">Email: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="email"
+                                    />
+                                    @error('email') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+                                <div class="form-group">
+                                    <label for="company_name">Username: </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        wire:model="username"
+                                    />
+                                    @error('username') <span class="alert alert-danger">{{$message}}</span>@enderror
+                                </div>
+
+                            </div>
+
+                            <!-- Modal footer -->
+                            <div class="modal-footer">
+                                <button
+                                    type="submit"
+                                    class="btn btn-primary"
+                                >Save</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            @endif
+
+        </div>
     </div>
     <div class="card-body">
         <div class="form">
@@ -24,6 +287,7 @@
                         <td>Location</td>
                         <td>Person In Charge</td>
                         <td>Email</td>
+                        <td>Detail</td>
                     </tr>
                 </thead>
                 <tbody>
@@ -45,14 +309,23 @@
                                 <td>
                                     {{$c->users[0]->email}}
                                 </td>
+                                <td>
+                                    <button
+                                        class="btn btn-info"
+                                        type="button"
+                                        wire:click="editCompany({{$c->id}})"
+                                    >
+                                        <span class="fas fa-info"></span>
+                                    </button>
+                                </td>
                             </tr>
                         @endforeach
                     @else
-
+                        <tr>
+                            <td colspan="6"></td>
+                        </tr>
                     @endif
-                    <tr>
 
-                    </tr>
                 </tbody>
             </table>
         </div>
